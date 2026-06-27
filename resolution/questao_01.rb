6897db7cc327ccd71a2d55bf3d179bae4592df27
Mauro <mauro_biazutti@hotmail.com>
2026-06-27 19:52:06 -0300

    feat: questão_01 implementando função solução

--- resolution/questao_01.rb
+++ resolution/questao_01.rb
@@ -4,38 +4,54 @@ file_content = File.read('resolution/dados_input.rb')
 file_binding = binding
 file_binding.eval(file_content)
 
+# def solucao(arg)
+#   format_dados = arg.map do |item|
+#     name = if item[:name].to_s.length < 11
+#              item[:name].to_s.ljust(11)
+#            else
+#              item[:name].to_s[0, 11]
+#            end
+
+#     cpf = if item[:cpf].to_s.length < 11
+#             item[:cpf].to_s.ljust(11)
+#           else
+#             item[:cpf].to_s[0, 11]
+#           end
+
+#     state = if item[:state].to_s.length < 11
+#               item[:state].to_s.ljust(11)
+#             else
+#               item[:state].to_s[0, 11]
+#             end
+
+#     value = if item[:value].to_s.length < 11
+#               item[:value].to_s.ljust(11)
+#             else
+#               item[:value].to_s[0, 11]
+#             end
+#     "#{name}#{cpf}#{state}#{value}"
+#   end
+#   puts format_dados.join("\n")
+# end
+
+###################### Solução com .map #########################
+
 def solucao(arg)
-  format_dados = arg.map do |item|
-    name = if item[:name].to_s.length < 11
-             item[:name].to_s.ljust(11)
-           else
-             item[:name].to_s[0, 11]
-           end
-
-    cpf = if item[:cpf].to_s.length < 11
-            item[:cpf].to_s.ljust(11)
-          else
-            item[:cpf].to_s[0, 11]
-          end
-
-    state = if item[:state].to_s.length < 11
-              item[:state].to_s.ljust(11)
-            else
-              item[:state].to_s[0, 11]
-            end
-
-    value = if item[:value].to_s.length < 11
-              item[:value].to_s.ljust(11)
-            else
-              item[:value].to_s[0, 11]
-            end
-    "#{name}#{cpf}#{state}#{value}"
-  end
-  puts format_dados.join("\n")
+  arg.map do |item|
+    # Transforma os valores em string, trunca em 11 caracteres e preenche com espaços se faltar
+    name  = item[:name].to_s[0...11].ljust(11)
+    cpf   = item[:cpf].to_s[0...11].ljust(11)
+    state = item[:state].to_s[0...11].ljust(11)
+    value = item[:value].to_s[0...11].ljust(11)
+    
+    # Junta os campos da linha
+    puts "#{name}#{cpf}#{state}#{value}"
+  end.join("\n") # Junta todas as linhas quebrando uma linha entre elas
 end
+
 solucao(file_binding.eval(file_content))
 
-#Saida
+# Saida
 # Maria Neusa97905796671Sao Paulo  1234
 # Ricardo Fon44010762900Rio Grande 567
 # Rick       80107290   Rio        157
